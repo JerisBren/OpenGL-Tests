2955ce080c5080a4090a6769ba9969d530ba1915
Thiago Henrique Angst <ThiagoAngst@hotmail.com>
2015-02-14 13:04:31 -0200

    Triangle

--- OpenGL - Tests/GLWindow.cpp
+++ OpenGL - Tests/GLWindow.cpp
@@ -15,9 +15,33 @@ GLWindow::~GLWindow()
 void GLWindow::initializeGL()
 {
 	glewInit();
+
+	GLfloat verts[]
+	{
+		+0.0f, +0.0f,
+		+1.0f, +1.0f,
+		-1.0f, +1.0f,
+
+		+0.0f, +0.0f,
+		-1.0f, -1.0f,
+		+1.0f, -1.0f,
+	};
+
+	GLuint myBufferID;
+	glGenBuffers(1, &myBufferID);
+
+	glBindBuffer(GL_ARRAY_BUFFER, myBufferID);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
+				 verts, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 }
 
 void GLWindow::paintGL()
 {
-
+	glViewport(0, 0, width(), height());
+	glDrawArrays(GL_TRIANGLES, 0, 6);
 }
